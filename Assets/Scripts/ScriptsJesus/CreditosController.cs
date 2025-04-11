0b8c7399ed0a9cd95c706abdc81b114b4a5137e3
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2025-04-10 20:40:34 -0500

    Menu Principal terminado al 100 %

--- Assets/Scripts/ScriptsJesus/CreditosController.cs
+++ Assets/Scripts/ScriptsJesus/CreditosController.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;  // Necesario para trabajar con los botones
 using UnityEngine.SceneManagement;
 
 public class CreditosController : MonoBehaviour
 {
+    // Variable pública para el nombre de la escena del menú
+    public string nombreEscenaMenu = "MenuPrincipalJesus";       // Puedes cambiarlo en el Inspector
+
+
+    // Referencia pública al botón en el Inspector
+    public Button volverButton;
+
+    void Start()
+    {
+        // Verificar que el botón esté asignado desde el Inspector
+        if (volverButton != null)
+        {
+            // Asignamos la función que se ejecutará cuando se haga clic en el botón
+            volverButton.onClick.AddListener(VolverAlMenu);
+        }
+        else
+        {
+            Debug.LogError("¡El botón no ha sido asignado en el Inspector!");
+        }
+    }
+
+
+    // Método para volver al menú principal
     public void VolverAlMenu()
     {
-        SceneManager.LoadScene("MenuPrincipalJesus"); //  Volver a  "MenuPrincipalJesus" 
+        SceneManager.LoadScene(nombreEscenaMenu); // Carga la escena del menú
     }
 }
--- Assets/Scripts/ScriptsJesus/MenuController.cs
+++ Assets/Scripts/ScriptsJesus/MenuController.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;  // Necesario para trabajar con los botones
 using UnityEngine.SceneManagement;  // Necesario para cargar escenas
 
 public class MenuController : MonoBehaviour
 {
-    // Este método se llama cuando el jugador presiona "Inicio"
-    public void StartGame()
+    // Variables públicas para asignar los botones desde el Inspector
+    public Button jugarButton;
+    public Button creditosButton;
+    public Button salirButton;
+
+    // Variables para los nombres de las escenas, se pueden cambiar desde el Inspector
+    public string nombreEscenaJuego = "Jesus";
+    public string nombreEscenaCreditos = "CreditosJesus";
+
+    void Start()
     {
-        SceneManager.LoadScene("Jesus");  // Cambia "GameScene" al nombre de tu escena de juego
+        // Asignar listeners a los botones, sin necesidad de usar OnClick() en el Inspector
+        jugarButton.onClick.AddListener(StartGame);
+        creditosButton.onClick.AddListener(ShowCredits);
+        salirButton.onClick.AddListener(QuitGame);
     }
 
-    // Este método se llama cuando el jugador presiona "Salir"
+
+    // Método para iniciar el juego
+    public void StartGame()
+    {
+        SceneManager.LoadScene(nombreEscenaJuego);  // Carga la escena especificada
+    }
+
+    // Método para salir del juego
     public void QuitGame()
     {
         Debug.Log("Salir del juego");
-        Application.Quit();  // Cierra el juego
+        Application.Quit();  // Cierra la aplicación
     }
 
+    // Método para mostrar los créditos
     public void ShowCredits()
     {
-        SceneManager.LoadScene("CreditosJesus");  // Cambia esto al nombre exacto de tu escena de créditos
+        SceneManager.LoadScene(nombreEscenaCreditos);  // Carga la escena de créditos
     }
 }
