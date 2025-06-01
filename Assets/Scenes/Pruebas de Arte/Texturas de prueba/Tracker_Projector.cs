d288c218b8fd353710bad9f48710f562c10ece04
AndreiMore <andreiloayza14@gmail.com>
2025-06-01 00:00:55 -0500

    Efecto del marcador del tracker terminado

--- Assets/Scenes/Pruebas de Arte/Texturas de prueba/Tracker_Projector.cs
+++ Assets/Scenes/Pruebas de Arte/Texturas de prueba/Tracker_Projector.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Tracker_Projector : MonoBehaviour
 {
-    public Light spotLight;            // Tu luz con cookie
-    public Transform eyeTransform;     // Transform del "ojo" del personaje
-    public float heightOffset = 0.1f;  // Qué tan lejos de la superficie
+    [SerializeField] Transform MarkerProjected;
+    [SerializeField] float Rotation_Speed;
 
-    void LateUpdate()
+
+    private void Update()
     {
-        // Hacer que la luz siga al ojo y siempre mire hacia abajo
-        spotLight.transform.position = eyeTransform.position;
-        spotLight.transform.rotation = Quaternion.Euler(90f, 0f, 0f); // Siempre mira hacia abajo
+        if (MarkerProjected != null) 
+        {
+            transform.rotation = Quaternion.LookRotation(MarkerProjected.position - transform.position);
+
+            float y = MarkerProjected.eulerAngles.y + Rotation_Speed * Time.deltaTime;
+            MarkerProjected.rotation = Quaternion.Euler(90, y, 0);
+        }
     }
 }
