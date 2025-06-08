0c5cc2a58d750287dddb4abe0f369492941e43d6
AndreiMore <andreiloayza14@gmail.com>
2025-06-07 20:45:27 -0500

    Organizacion de prefabs de VFX 1

--- Assets/Scenes/Pruebas de Arte/Texturas de prueba/Scripts/Disparador_PruebasArte.cs
+++ Assets/Scenes/Pruebas de Arte/Texturas de prueba/Scripts/Disparador_PruebasArte.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Disparador_PruebasArte : MonoBehaviour
 {
     [SerializeField] GameObject Effect;
+    [SerializeField] GameObject MuzzleFlash;
     [SerializeField] float vel;
 
     private void Update()
@@ -18,6 +19,12 @@ public class Disparador_PruebasArte : MonoBehaviour
             Rigidbody rb = proyectil.GetComponent<Rigidbody>();
 
             rb.velocity = new Vector3 (vel, 0, 0);
+
+            if(MuzzleFlash != null)
+            {
+                GameObject muzzle = Instantiate(MuzzleFlash);
+                muzzle.transform.position = transform.position;
+            }
         }
     }
 }
