3ccd6ca6ca0e8e28a0cf103d218486bbdac19786
AndreiMore <andreiloayza14@gmail.com>
2025-07-07 13:37:50 -0500

    Pruebas del loot y el VFX Glitchtime

---
+++ Assets/Scenes/Pruebas de Arte/Texturas de prueba/Scripts/PlayerPos_Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PlayerPos_Test : MonoBehaviour
+{
+    [SerializeField] VisualEffect VFX;
+    [SerializeField] Transform PlayerPos;
+
+    private void Update()
+    {
+        if (VFX != null && PlayerPos != null)
+        {
+            VFX.SetVector3("Player Position", PlayerPos.position);
+        }
+    }
+
+
+}
