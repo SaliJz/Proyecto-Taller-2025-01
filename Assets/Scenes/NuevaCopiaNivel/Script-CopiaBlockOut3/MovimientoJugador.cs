d73b3c0be3cee500c4480d6c2f5a10bb660a41d8
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2025-07-08 22:02:57 -0500

    Creando copia de Blockout3

---
+++ Assets/Scenes/NuevaCopiaNivel/Script-CopiaBlockOut3/MovimientoJugador.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public float velocidadMovimiento = 5f;
+    private Rigidbody rb;
+    private Vector3 movimiento;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+       
+        float movimientoX = Input.GetAxisRaw("Horizontal");
+        float movimientoZ = Input.GetAxisRaw("Vertical");
+
+       
+        movimiento = new Vector3(movimientoX, 0f, movimientoZ).normalized;
+    }
+
+    void FixedUpdate()
+    {
+
+        rb.velocity = movimiento * velocidadMovimiento + new Vector3(0, rb.velocity.y, 0);
+    }
+
+}
