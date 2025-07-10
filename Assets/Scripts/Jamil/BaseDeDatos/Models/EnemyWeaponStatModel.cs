bbb03d029e63f99262a324a42ace24878bfbda69
JamilCerron <luis.cerron.2010@gmail.com>
2025-07-10 13:47:41 -0500

    Añadido script Get_Top_Weapon_Per_Enemy para obtener el array de enemigos muertos con determinada arma, añadido script LevelManager_SQL que actualiza, separa y asigna datos en cada nivel, añadidos scripts EnemyWeaponStatModel y SelectEnemyWeaponStatModel para almacenar y recolectar datos de los archivos PHP

---
+++ Assets/Scripts/Jamil/BaseDeDatos/Models/EnemyWeaponStatModel.cs
@@ -0,0 +1,8 @@
+[System.Serializable]
+public class EnemyWeaponStatModel
+{
+    public int enemy_weapon_id;
+    public string enemy_type;
+    public string weapon_name;
+    public int kill_count;
+}
