d16b2294893fb231baaa47da6b7c3d482cbfe052
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2025-07-10 23:39:46 -0500

    Updating the UpdatePosition script

---
+++ Assets/Base de Datos Tanaka/Script/UpdatePosition.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UpdatePosition : MonoBehaviour
+{
+    public int player_level_id = 1; 
+    public Vector3 nuevaPosicion;
+
+    private string url = "http://localhost/juego/Update_player_position.php";
+
+    public void Actualizar()
+    {
+        StartCoroutine(EnviarUpdate(nuevaPosicion));
+    }
+
+    IEnumerator EnviarUpdate(Vector3 pos)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("player_level_id", player_level_id);
+        form.AddField("axisX", pos.x.ToString("F3"));
+        form.AddField("axisY", pos.y.ToString("F3"));
+        form.AddField("axisZ", pos.z.ToString("F3"));
+
+        UnityWebRequest www = UnityWebRequest.Post(url, form);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Error al actualizar datos: " + www.error);
+        }
+        else
+        {
+            Debug.Log("Respuesta del servidor: " + www.downloadHandler.text);
+        }
+    }
+
+}
