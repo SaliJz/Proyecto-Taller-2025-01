ce5bb5818b6dd0b148b3ef08260d1d3d21dec294
Jean Flores <jeanpierre194fp@gmail.com>
2025-07-14 19:20:16 -0500

    PasarEscenasJP

--- Assets/Scripts/JeanPierre/JefeSerpiente/ControladorDeEscenas/SceneSwitcher.cs
+++ Assets/Scripts/JeanPierre/JefeSerpiente/ControladorDeEscenas/SceneSwitcher.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitcher : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // En el Inspector de Unity podrás asignar el nombre de la escena que quieres cargar
     [SerializeField] private string escenaParaTecla8;
