634a49b993601542db8570243c903281eeac7047
JamilCerron <luis.cerron.2010@gmail.com>
2025-07-25 18:18:43 -0500

    HaloCorregido

--- Assets/Scenes/Pruebas de Arte/Texturas de prueba/Scripts/Destroy_VFX.cs
+++ Assets/Scenes/Pruebas de Arte/Texturas de prueba/Scripts/Destroy_VFX.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Destroy_VFX : MonoBehaviour
 {
     public float Destroy_Delay;
+    public VisualEffect vfx;
     void Start()
     {
+        vfx = GetComponent<VisualEffect>();
+        vfx.playRate = 1.5f; 
         StartCoroutine(delay());
     }
-
+   
     IEnumerator delay()
     {
         yield return new WaitForSeconds(Destroy_Delay);
